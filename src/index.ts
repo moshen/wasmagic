bff5bccb02341b917465a0e7fcc298e7f0638a34
Warnar Boekkooi <warnar.boekkooi@lengoo.de>
2024-01-26 11:07:40 +0100

    Use JSDocs
    This allows editors to display a description nicely.

--- src/index.ts
+++ src/index.ts
@@ -3,38 +3,68 @@ import libmagicFactory from "../dist/libmagic-wrapper";
 declare function wrappedGetMime(bufPointer: number, bufLength: number): string;
 
 export enum WASMagicFlags {
-  NONE=0x0000000 /* No flags */,
-  DEBUG=0x0000001 /* Turn on debugging */,
-  SYMLINK=0x0000002 /* Follow symlinks */,
-  COMPRESS=0x0000004 /* Check inside compressed files */,
-  DEVICES=0x0000008 /* Look at the contents of devices */,
-  MIME_TYPE=0x0000010 /* Return the MIME type */,
-  CONTINUE=0x0000020 /* Return all matches */,
-  CHECK=0x0000040 /* Print warnings to stderr */,
-  PRESERVE_ATIME=0x0000080 /* Restore access time on exit */,
-  RAW=0x0000100 /* Don't convert unprintable chars */,
-  ERROR=0x0000200 /* Handle ENOENT etc as real errors */,
-  MIME_ENCODING=0x0000400 /* Return the MIME encoding */,
-  APPLE=0x0000800 /* Return the Apple creator/type */,
-  EXTENSION=0x1000000 /* Return a /-separated list of extensions */,
-  COMPRESS_TRANSP=0x2000000 /* Check inside compressed files but not report compression */,
-  NO_COMPRESS_FORK=0x4000000 /* Don't allow decompression that needs to fork */,
-  NO_CHECK_COMPRESS=0x0001000 /* Don't check for compressed files */,
-  NO_CHECK_TAR=0x0002000 /* Don't check for tar files */,
-  NO_CHECK_SOFT=0x0004000 /* Don't check magic entries */,
-  NO_CHECK_APPTYPE=0x0008000 /* Don't check application type */,
-  NO_CHECK_ELF=0x0010000 /* Don't check for elf details */,
-  NO_CHECK_TEXT=0x0020000 /* Don't check for text files */,
-  NO_CHECK_CDF=0x0040000 /* Don't check for cdf files */,
-  NO_CHECK_CSV=0x0080000 /* Don't check for CSV files */,
-  NO_CHECK_TOKENS=0x0100000 /* Don't check tokens */,
-  NO_CHECK_ENCODING=0x0200000 /* Don't check text encodings */,
-  NO_CHECK_JSON=0x0400000 /* Don't check for JSON files */,
-  NO_CHECK_SIMH=0x0800000 /* Don't check for SIMH tape files */,
+  /** No flags */
+  NONE = 0x0000000,
+  /** Turn on debugging */
+  DEBUG = 0x0000001,
+  /** Follow symlinks */
+  SYMLINK = 0x0000002,
+  /** Check inside compressed files */
+  COMPRESS = 0x0000004,
+  /** Look at the contents of devices */
+  DEVICES = 0x0000008,
+  /** Return the MIME type */
+  MIME_TYPE = 0x0000010,
+  /** Return all matches */
+  CONTINUE = 0x0000020,
+  /** Print warnings to stderr */
+  CHECK = 0x0000040,
+  /** Restore access time on exit */
+  PRESERVE_ATIME = 0x0000080,
+  /** Don't convert unprintable chars */
+  RAW = 0x0000100,
+  /** Handle ENOENT etc as real errors */
+  ERROR = 0x0000200,
+  /** Return the MIME encoding */
+  MIME_ENCODING = 0x0000400,
+  /** Return the Apple creator/type */
+  APPLE = 0x0000800,
+  /** Return a /-separated list of extensions */
+  EXTENSION = 0x1000000,
+  /** Check inside compressed files but not report compression */
+  COMPRESS_TRANSP = 0x2000000,
+  /** Don't allow decompression that needs to fork */
+  NO_COMPRESS_FORK = 0x4000000,
+  /** Don't check for compressed files */
+  NO_CHECK_COMPRESS = 0x0001000,
+  /** Don't check for tar files */
+  NO_CHECK_TAR = 0x0002000,
+  /** Don't check magic entries */
+  NO_CHECK_SOFT = 0x0004000,
+  /** Don't check application type */
+  NO_CHECK_APPTYPE = 0x0008000,
+  /** Don't check for elf details */
+  NO_CHECK_ELF = 0x0010000,
+  /** Don't check for text files */
+  NO_CHECK_TEXT = 0x0020000,
+  /** Don't check for cdf files */
+  NO_CHECK_CDF = 0x0040000,
+  /** Don't check for CSV files */
+  NO_CHECK_CSV = 0x0080000,
+  /** Don't check tokens */
+  NO_CHECK_TOKENS = 0x0100000,
+  /** Don't check text encodings */
+  NO_CHECK_ENCODING = 0x0200000,
+  /** Don't check for JSON files */
+  NO_CHECK_JSON = 0x0400000,
+  /** Don't check for SIMH tape files */
+  NO_CHECK_SIMH = 0x0800000,
 }
 
 export class WASMagic {
-  static async create(flag: WASMagicFlags = WASMagicFlags.MIME_TYPE): Promise<WASMagic> {
+  static async create(
+    flag: WASMagicFlags = WASMagicFlags.MIME_TYPE,
+  ): Promise<WASMagic> {
     const Module = await libmagicFactory();
     return new WASMagic(Module, flag);
   }

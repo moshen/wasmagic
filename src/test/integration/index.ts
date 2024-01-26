4109675ed135f1e0a0513e737507f14cd892caa4
Warnar Boekkooi <warnar.boekkooi@lengoo.de>
2024-01-26 10:21:14 +0100

    Add Text for WASMagicFlags.NONE
    Show that the flags work as expected.

--- src/test/integration/index.ts
+++ src/test/integration/index.ts
@@ -2,11 +2,12 @@ import * as fs from "fs";
 import * as path from "path";
 import { glob } from "glob";
 import { cases } from "./data";
-import { WASMagic } from "../../index";
+import { WASMagic, WASMagicFlags } from "../../index";
 
 describe("WASMagic", () => {
   let fileExampleList: string[];
   let magic: WASMagic;
+  let magicText: WASMagic;
 
   beforeAll(async () => {
     const xbyxRegexp = /[0-9]+x[0-9]+/;
@@ -18,20 +19,31 @@ describe("WASMagic", () => {
       .sort()
       .filter((f) => !xbyxRegexp.test(f));
     magic = await WASMagic.create();
+    magicText = await WASMagic.create(WASMagicFlags.NONE);
   });
 
   // Dummy test to create test table output
-  test("Create Table", () => {
+  test.skip("Create Table", () => {
     const out: string[][] = [];
     for (const file of fileExampleList) {
       const fileBuf = fs.readFileSync(file);
-      const t = magic.getMime(fileBuf);
-      out.push([path.relative(path.resolve(__dirname, "../../../"), file), t]);
+      const mimeType = magic.getMime(fileBuf);
+      const textType = magicText.getMime(fileBuf);
+      out.push([
+        path.relative(path.resolve(__dirname, "../../../"), file),
+        mimeType,
+        textType,
+      ]);
     }
-    // console.log(JSON.stringify(out, undefined, 2));
+    console.log(JSON.stringify(out, undefined, 2));
   });
 
-  test.each(cases)("%s identified as %s", (file, expectedOutput) => {
-    expect(magic.getMime(fs.readFileSync(file))).toBe(expectedOutput);
-  });
+  test.each(cases)(
+    "%s identified as %s",
+    (file, expectedOutput, expectedText) => {
+      const fileBuf = fs.readFileSync(file);
+      expect(magic.getMime(fileBuf)).toBe(expectedOutput);
+      expect(magicText.getMime(fileBuf)).toBe(expectedText);
+    },
+  );
 });

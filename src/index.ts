9c32c6b7bf5237e0349eaeec8671369d1b2eef0b
Warnar Boekkooi <warnar.boekkooi@lengoo.de>
2024-02-13 22:44:16 -0500

    Feature add magic flags
    Closes #30
    
    In order to allow more extensive usage of LibMagic we now expose the
    Flags (https://github.com/file/file/blob/46da4cf/src/magic.h.in#L32).
    Where the default is to use `MAGIC_MIME_TYPE`.
    
    - Force UTC Timezone for tests. This avoids errors when GitHub runner
      are running in another timezone

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,81 @@ import libmagicFactory from "../dist/libmagic-wrapper";
 
 declare function wrappedGetMime(bufPointer: number, bufLength: number): string;
 
+export enum WASMagicFlags {
+  /** No flags */
+  NONE = 0x0000000,
+  /** Turn on debugging */
+  DEBUG = 0x0000001,
+  /** Follow symlinks */
+  SYMLINK = 0x0000002,
+  /** Check inside compressed files */
+  COMPRESS = 0x0000004,
+  /** Look at the contents of devices */
+  DEVICES = 0x0000008,
+  /** Return the MIME type */
+  MIME_TYPE = 0x0000010,
+  /** Return all matches */
+  CONTINUE = 0x0000020,
+  /** Print warnings to stderr */
+  CHECK = 0x0000040,
+  /** Restore access time on exit */
+  PRESERVE_ATIME = 0x0000080,
+  /** Don't convert unprintable chars */
+  RAW = 0x0000100,
+  /** Handle ENOENT etc as real errors */
+  ERROR = 0x0000200,
+  /** Return the MIME encoding */
+  MIME_ENCODING = 0x0000400,
+  /** Return the Apple creator/type */
+  APPLE = 0x0000800,
+  /** Return a /-separated list of extensions */
+  EXTENSION = 0x1000000,
+  /** Check inside compressed files but not report compression */
+  COMPRESS_TRANSP = 0x2000000,
+  /** Don't allow decompression that needs to fork */
+  NO_COMPRESS_FORK = 0x4000000,
+  /** Don't check for compressed files */
+  NO_CHECK_COMPRESS = 0x0001000,
+  /** Don't check for tar files */
+  NO_CHECK_TAR = 0x0002000,
+  /** Don't check magic entries */
+  NO_CHECK_SOFT = 0x0004000,
+  /** Don't check application type */
+  NO_CHECK_APPTYPE = 0x0008000,
+  /** Don't check for elf details */
+  NO_CHECK_ELF = 0x0010000,
+  /** Don't check for text files */
+  NO_CHECK_TEXT = 0x0020000,
+  /** Don't check for cdf files */
+  NO_CHECK_CDF = 0x0040000,
+  /** Don't check for CSV files */
+  NO_CHECK_CSV = 0x0080000,
+  /** Don't check tokens */
+  NO_CHECK_TOKENS = 0x0100000,
+  /** Don't check text encodings */
+  NO_CHECK_ENCODING = 0x0200000,
+  /** Don't check for JSON files */
+  NO_CHECK_JSON = 0x0400000,
+  /** Don't check for SIMH tape files */
+  NO_CHECK_SIMH = 0x0800000,
+}
+
 export type WASMagicOptions = {
+  flags?: WASMagicFlags;
   loadDefaultMagicfile?: boolean;
   magicFiles?: Uint8Array[];
   stdio?: StdioOverrideFunction;
 };
 
 type WASMagicOptionsComplete = {
+  flags: WASMagicFlags;
   loadDefaultMagicfile: boolean;
   magicFiles: Uint8Array[];
   stdio: StdioOverrideFunction;
 };
 
 const defaultWASMagicOptions: WASMagicOptionsComplete = Object.freeze({
+  flags: WASMagicFlags.MIME_TYPE,
   loadDefaultMagicfile: true,
   magicFiles: [],
   stdio: (_stdioName: "stdout" | "stderr", _text: string) => {},
@@ -52,8 +114,8 @@ export class WASMagic {
     const loadErr = Module.ccall(
       "magic_wrapper_load",
       "string",
-      ["string"],
-      [magicPaths.join(":")],
+      ["number", "string"],
+      [options.flags, magicPaths.join(":")],
     );
 
     if (loadErr !== "") {
--- src/test/integration/index.ts
+++ src/test/integration/index.ts
@@ -2,12 +2,13 @@ import * as fs from "fs";
 import * as path from "path";
 import { glob } from "glob";
 import { cases } from "./data";
-import { WASMagic } from "../../index";
+import { WASMagic, WASMagicFlags } from "../../index";
 
 describe("WASMagic", () => {
   describe("Default magic file", () => {
     let fileExampleList: string[];
     let magic: WASMagic;
+    let magicText: WASMagic;
 
     beforeAll(async () => {
       const xbyxRegexp = /[0-9]+x[0-9]+/;
@@ -19,25 +20,35 @@ describe("WASMagic", () => {
         .sort()
         .filter((f) => !xbyxRegexp.test(f));
       magic = await WASMagic.create();
+      magicText = await WASMagic.create({
+        flags: WASMagicFlags.NONE,
+      });
     });
 
     // Dummy test to create test table output
-    test("Create Table", () => {
+    test.skip("Create Table", () => {
       const out: string[][] = [];
       for (const file of fileExampleList) {
         const fileBuf = fs.readFileSync(file);
-        const t = magic.getMime(fileBuf);
+        const mimeType = magic.getMime(fileBuf);
+        const textType = magicText.getMime(fileBuf);
         out.push([
           path.relative(path.resolve(__dirname, "../../../"), file),
-          t,
+          mimeType,
+          textType,
         ]);
       }
-      // console.log(JSON.stringify(out, undefined, 2));
+      console.log(JSON.stringify(out, undefined, 2));
     });
 
-    test.each(cases)("%s identified as %s", (file, expectedOutput) => {
-      expect(magic.getMime(fs.readFileSync(file))).toBe(expectedOutput);
-    });
+    test.each(cases)(
+      "%s identified as %s",
+      (file, expectedOutput, expectedText) => {
+        const fileBuf = fs.readFileSync(file);
+        expect(magic.getMime(fileBuf)).toBe(expectedOutput);
+        expect(magicText.getMime(fileBuf)).toBe(expectedText);
+      },
+    );
   });
 
   describe("Specified foobar custom magic file, with default magic file", () => {

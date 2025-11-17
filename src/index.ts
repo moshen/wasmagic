4c0ec973911379352c33aafa55d02c0236f0e38c
Colin Kennedy <moshen.colin@gmail.com>
2025-11-17 18:09:47 -0500

    Changes before merging locatefile change from #53 (#55)
    - Add option to override locateFile for loading WASM files from custom sources (e.g., local path or remote URL)
    
    Co-authored-by: Ron Korland <1866339+ronkorland@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -64,11 +64,14 @@ export enum WASMagicFlags {
   NO_CHECK_SIMH = 0x0800000,
 }
 
+export type LibmagicLocateFile = LibmagicModule["locateFile"];
+
 export type WASMagicOptions = {
   flags?: WASMagicFlags;
   loadDefaultMagicfile?: boolean;
   magicFiles?: Uint8Array[];
   stdio?: StdioOverrideFunction;
+  locateFile?: LibmagicLocateFile;
 };
 
 type WASMagicOptionsComplete = {
@@ -89,7 +92,9 @@ export class WASMagic {
   static async create(
     options: WASMagicOptions = defaultWASMagicOptions,
   ): Promise<WASMagic> {
-    const Module = await libmagicFactory();
+    const Module = await libmagicFactory({
+      locateFile: options.locateFile,
+    });
     return new WASMagic(Module, options);
   }
 
